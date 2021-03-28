ec9cdc44bb02f6b037bd971b87d8f07025332978
Lim Chee Kin <limcheekin@vobject.com>
2021-03-28 17:24:36 +0800

    first import of flutter_modular navigation

--- lib/main.dart
+++ lib/main.dart
@@ -2,59 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:nested_list/nested_list_screen.dart';
 import 'package:showcase/showcase_screen.dart';
 import 'package:tab_buttons/tab_buttons_screen.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'string.dart';
 
-void main() {
-  runApp(MyApp());
+final AppModule appModule = AppModule();
+void main() => runApp(ModularApp(module: appModule, child: AppWidget()));
+
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [];
+
+  @override
+  final List<ModularRoute> routes = [
+    // ChildRoute(Modular.initialRoute, child: (context, args) => HomeScreen()),
+    ChildRoute('/', child: (_, __) => HomeScreen()),
+    ChildRoute('/nested_list', child: (_, __) => NestedListScreen()),
+    ChildRoute('/tab_buttons', child: (_, __) => TabButtonsScreen()),
+    ChildRoute('/showcase', child: (_, __) => ShowcaseScreen()),
+  ];
 }
 
-class MyApp extends StatelessWidget {
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<String> names = [
-      'Nested List',
-      'Tab Buttons',
-      'Showcase',
-    ];
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Flutter Widgets'),
-          ),
-          body: Center(
-            child: ListView.builder(
-              itemCount: names.length,
-              itemBuilder: (BuildContext context, int position) {
-                var name = names[position];
-                return Card(
-                  margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
-                  child: ListTile(
-                    title: Text(name),
-                    trailing: Icon(Icons.keyboard_arrow_right),
-                    onTap: () {
-                      switch (position) {
-                        case 0:
-                          _gotoScreen(context, NestedListScreen());
-                          break;
-                        case 1:
-                          _gotoScreen(context, TabButtonsScreen());
-                          break;
-                        case 2:
-                          _gotoScreen(context, ShowcaseScreen());
-                          break;
-                        default:
-                      }
-                    },
-                  ),
-                );
-              },
-            ),
-          )),
-    );
+      title: 'Flutter Widgets Explorer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    ).modular();
   }
+}
 
-  void _gotoScreen(BuildContext context, Widget screen) {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (_) => screen,
-    ));
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Widgets'),
+      ),
+      body: ListView.builder(
+        itemCount: appModule.routes.length - 1,
+        itemBuilder: (BuildContext context, int position) {
+          final route = appModule.routes[position + 1];
+          return Card(
+            margin: EdgeInsets.fromLTRB(8, 4, 8, 4),
+            child: ListTile(
+              title: Text(route.routerName.substring(1).toTitleCase()),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: () => Modular.to.navigate(route.routerName),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
